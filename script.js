2362daaa3f17c31041aad84652ddabe832243066
Leodz-debug <leo11diazmendez@gmail.com>
2026-03-31 23:28:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-const sections = document.querySelectorAll('.section');
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
-    }
-  });
-});
-
-sections.forEach(section => {
-  section.style.opacity = 0;
-  section.style.transform = "translateY(40px)";
-  observer.observe(section);
-});
-
-// Dark mode
 const toggle = document.getElementById("themeToggle");
 
-toggle.addEventListener("click", () => {
+toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark");
-});
-
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navbar.style.backdropFilter = "blur(15px)";
-  } else {
-    navbar.style.backdropFilter = "blur(10px)";
-  }
 });

e607c6c7e7e845c9d339d161a6b85fe89f16776a
Leodz-debug <leo11diazmendez@gmail.com>
2026-04-01 00:04:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const toggle = document.getElementById("themeToggle");
+const navbar = document.querySelector(".navbar");
 
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark");
+});
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  if (window.innerWidth > 768) {
+    navbar.classList.remove("hide");
+    return;
+  }
+
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    navbar.classList.remove("hide");
+    lastScroll = currentScroll;
+    return;
+  }
+
+  if (currentScroll > lastScroll && currentScroll > 80) {
+    navbar.classList.add("hide");
+  } else {
+    navbar.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
 });
